1919affa1c7a279ddbe25c1417726412d02535c4
Tarek Rayan <tarekrayyan50@gmail.com>
2024-06-27 11:35:09 +0300

    ALU Package

--- alu_pkg.sv
+++ alu_pkg.sv
@@ -2,7 +2,7 @@ package alu_pkg;
     import uvm_pkg ::*;
     `include "uvm_macros.svh"
     typedef enum bit[1:0] {AND,NAND,OR,XOR} operation_a;
-    typedef enum bit[1:0] {XNOR,AND,NOR,OR} operation_b;
-    
-    typedef uvm_sequencer sequencer;
+    typedef enum bit[1:0] {XNOR,AND_OP_B,NOR,OR_OP_B} operation_b;
+
+    typedef uvm_sequencer#(sequence_item) sequencer;
 endpackage 
