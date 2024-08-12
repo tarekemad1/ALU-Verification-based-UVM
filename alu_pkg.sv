dfbd9201cb8162c0aff9e6821aed514aa2d13d1c
Tarek Rayan <tarekrayyan50@gmail.com>
2024-08-12 11:36:20 +0300

    ALU Package added

---
+++ alu_pkg.sv
@@ -0,0 +1,21 @@
+package alu_pkg;
+    import uvm_pkg ::*;
+    `include "uvm_macros.svh";
+    `include "sequence_item.svh";
+    `include "sequencer.svh";
+    `include "driver.svh";
+    `include "monitor.svh";
+    `include "agent.svh";
+    `include "base_sequence.svh";
+    `include "reset_seq.svh";
+    `include "mode_A_seq.svh";
+    `include "mode_B_seq.svh";
+    `include "scoreboard.svh";
+    `include "coverage.svh";
+    `include "env.svh";
+    `include "base_test.svh";
+    `include "mode_A_test.svh";
+    `include "mode_B_test.svh";
+    `include "full_test.svh";
+    
+endpackage 
